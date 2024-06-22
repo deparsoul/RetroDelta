a3b2e1655ac7da8a8c20bcabce9b9c776e875c53
deparsoul <2436566+deparsoul@users.noreply.github.com>
2024-06-22 22:35:20 +0800

    switch skin

---
+++ example/advanced/switching-d-p/README.md
@@ -0,0 +1,15 @@
+# Advanced Usage - Switch Between D-Pad and Joystick
+
+This is an example demonstrating how to combine multiple skins together and switch between different layouts with a switch button.
+In this example, both D-Pad and Joystick versions of skins are combined to support real-time switching.
+
+Please refer to `retrodelta.json` for the detailed setup.
+
+**Note:**
+- Switched skins should have the same screen position and size
+- To ensure the Joystick works as D-Pad properly, make sure to set Quick Menu => Controls => Port 1 => Analog to Digital Type => Left Analog
+
+## Information of the Original Skin
+
+- **Name**: [SOLOTHUMBS GBA](https://www.reddit.com/r/Delta_Emulator/comments/1cpzzsf/updated_delta_skin_gba_solo_thumbs/)
+- **Author**: [R3BEL](https://www.reddit.com/user/R3BEL85/)
--- index.js
+++ index.js
@@ -5,25 +5,40 @@ const RetroDelta = require('./lib/retrodelta');
 const util = require('./lib/util');
 const logger = util.logger;
 
-// logger.info(process.argv);
-
 const argv = process.argv;
-const basePath = argv[2];
+if (argv.length != 3) {
+  logger.info('\nUsage: node index "<Delta Skin Path>"\n');
+  process.exit(1);
+}
 
-const config = {
-  deltaPath: basePath,
-  outputPath: basePath,
-  scriptPath: __dirname,
+const basePath = argv[2];
+const configPath = path.join(basePath, 'retrodelta.json');
+
+const defaultConfig = {
+  deltaPath: '.',
+  outputPath: '.',
   templatePath: path.join(__dirname, 'template'),
   outputName: 'delta',
   debugName: 'delta-debug',
 };
+const config = { ...defaultConfig, ...util.readJson(configPath, true) };
+for (const key in config) {
+  if (key.endsWith('Path')) {
+    config[key] = path.resolve(basePath, config[key]);
+  }
+}
 logger.debug(config);
 
-const deltaInfo = util.readJson(path.join(config.deltaPath, 'info.json'));
-const retro = new RetroDelta(deltaInfo, config);
+const retro = new RetroDelta(config);
+
+if (config.switchSkin) {
+  const retros = config.switchSkin.map(deltaPath => new RetroDelta({ ...config, deltaPath: path.resolve(basePath, deltaPath) }));
+  retro.addSwitchSkin(retros);
+}
+
 retro.generateRetroShader();
 util.saveFile(retro.generateRetroConfig(), path.join(config.outputPath, `${config.outputName}.cfg`));
+
 if (config.debugName) {
   retro.addDebugOverlay();
   util.saveFile(retro.generateRetroConfig(), path.join(config.outputPath, `${config.debugName}.cfg`));
